d2cf5a116979406f938974c24cf837ed152fc6c5
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2010-06-03 08:04:32 +0900

    remove comment

--- lib/ramy.rb
+++ lib/ramy.rb
@@ -23,7 +23,6 @@ class Object
 end
 
 class String
-  # 改行コードをbrタグへ変換
   def to_br
     self.gsub(/\r\n/,'<br />').gsub(/(\r|\n)/,'<br />')
   end
@@ -31,10 +30,13 @@ end
 
 class Ramy
   def initialize(prefix)
+    @prefix = prefix
     @cgi = CGI.new
-    @cgi.params.merge!(CGI::parse(@cgi.query_string)){|key, self_val, other_val| self_val }
-    @session = start_session(@cgi, prefix, prefix+".")
-    
+    if @cgi.query_string
+      @cgi.params.merge(CGI::parse(@cgi.query_string)){|key, self_val, other_val| self_val }
+    end
+
+    @session = start_session(@cgi, @prefix, @prefix+".")
 
     @default_method = 'error'
     @methods = [:error]
@@ -43,7 +45,7 @@ class Ramy
 
     @title = ""
     @page_list = 50 # 1ページに表示する行数
-    @layout = true  # レイアウトの利用
+    @use_layout = true
   end
   def script_name
     @cgi.script_name
@@ -57,9 +59,11 @@ class Ramy
   end
   def start
     begin
-      if @method.to_s != "" && @methods.include?(@method.intern)
+      if @method.blank? 
+        raise "メソッドが指定されていません｡"
+      elsif @methods.include?(@method.intern)
         b = send(@method)
-        output_bind(@method,b,@layout)
+        output_bind(@method,b,@use_layout)
       else
         raise "不明なメソッドです｡ [#{@method}]"
       end
@@ -83,12 +87,10 @@ class Ramy
     end
     output_head({ 'status' => '302 Found', 'Location' => location})
   end
-  # 致命的エラー発生時エラーを画面に出力
   def fatal_error(error)
     output_head
     print "Fatal Error: #{error}"
   end
-  # ヘッダー出力
   def output_head(head=nil)
     if head
       print @cgi.header(head)
@@ -96,11 +98,10 @@ class Ramy
       print @cgi.header
     end
   end
-  # レイアウトが必要ない場合はlayout=falseにする
-  def output_bind(base,b,layout=true)
+  def output_bind(base,b,use_layout=true)
     title = @title
     main_html = get_rhtml(base).result(b)
-    if layout
+    if use_layout
       html = get_rhtml('application').result(binding)
     else
       html = main_html
@@ -133,34 +134,27 @@ class Ramy
     session_update
     str
   end
-  # エラーメッセージの取得
   def get_error
     str = pop_session('error')
     str.to_br
   end
-  # メッセージの取得
   def get_message
     pop_session('message')
   end
-  # メッセージ文字列のセット
   def set_message(str)
     @session['message'] = str
     session_update
   end
-  # エラー文字列のセット
   def set_error(str)
     @session['error'] = str
     session_update
   end
-  # エラーの存在確認
   def error_exist?
     session_exist?('error')
   end
-  # メッセージの存在確認
   def message_exist?
     session_exist?('message')
   end
-  # 指定したkeyのセッション値の存在確認
   def session_exist?(key)
     if @session[key].to_s != ""
       true
@@ -168,11 +162,9 @@ class Ramy
       false
     end
   end
-  # セッションのスタート
   def start_session(cgi,key,prefix)
     CGI::Session.new(cgi, "session_key" => key,"prefix" => prefix,"session_expires" => Time.now + 60*60*24*365)
   end
-  # 指定した複数keyのセッション削除
   def delete_session_key(key)
       @session[key] = nil
   end
@@ -181,15 +173,12 @@ class Ramy
       delete_session_key(key)
     }
   end
-  # セッションの削除
   def session_delete
     @session.delete
   end
-  # セッションのアップデート(保存)
   def session_update
     @session.update
   end
-  # 複数keyで値を取得
   def get_values(keys)
     res = Array.new
     keys.each{|key|
@@ -197,25 +186,22 @@ class Ramy
     }
     res
   end
-  # パラメータもしくはセッションデータから値を取得
   def get_value(key)
     value = param(key)
     if value
       if value == ""
         @session[key] = nil
       else
-        @session[key] = value # パラメータをセッションに保存
+        @session[key] = value
       end
     else
       value = @session[key]
     end
     value
   end
-  # パラメータの生データ取得
   def param_raw(keyword)
     @cgi.params[keyword][0]
   end
-  # パラメータの取得
   def param(keyword)
     value = param_raw(keyword)
     # ファイルUPなどでPOSTがmultipartの場合
@@ -225,7 +211,6 @@ class Ramy
       return value
     end
   end
-  # 複数keyでパラメータを取得
   def params(keys)
     res = Array.new
     keys.each{|key|
@@ -261,7 +246,6 @@ class Ramy
     end
     num_pages
   end
-  # 文字列の前後の空白除去､空文字､nilの場合はそのまま返す
   def Ramy.strip(value)
     unless value.blank?
       value.gsub(/(^(\s|　)+)|((\s|　)+$)/, '')
@@ -269,19 +253,15 @@ class Ramy
       value
     end
   end
-  # 文字列をHTMLコードへエスケープ
   def escape(value)
     CGI.escape(value)
   end
-  ########## デバッグ用 ##########
-  # パラメータ内容を文字列で返す
   def params_str
     @cgi.params.inspect + @session.inspect
   end
-  # Syslogへのログ出力
   def log(value)
     Syslog.open(@prefix)
-    Syslog.log(Syslog::LOG_WARNING,value.to_s, 100)
+    Syslog.log(Syslog::LOG_WARNING,value.to_s)
     Syslog.close
   end
 end
