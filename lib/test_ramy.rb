1d76e1c49b72d9c10101b30297c009ee03bf2a55
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2010-06-03 09:29:51 +0900

    add lib/test_ramy.rb

---
+++ lib/test_ramy.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'ramy'
+require 'test/unit'
+class TestRamy < Test::Unit::TestCase
+  def setup
+    ARGV.replace(["mt=error"])
+    @ramy = Ramy.new('ramy_test')
+    @ramy.start
+  end
+  def test_strip
+    values = [['2','2'],[' 2','2'],['2 ','2'],[' 2 ','2'],
+              ['　2','2'],['2　','2'],['　2　','2'],
+              ['さとみ','さとみ'],['　さとみ','さとみ'],['さとみ　','さとみ'],['　さとみ　','さとみ'],
+              ['',''],[nil,nil],[' ',''],['　',''],[' 　 ',''],['　 　',''],
+              ['高橋 義男','高橋 義男'],['高橋　義男','高橋　義男'],
+              [' 高橋 義男 ','高橋 義男'],['　高橋　義男　','高橋　義男'],
+              ['　高橋 義男　','高橋 義男'],[' 高橋　義男 ','高橋　義男']]
+    values.each do |input,res|
+      assert_equal(res,Ramy.strip(input),%Q!"#{input}" -> "#{res}"!)
+    end
+  end
+  def test_object_blank
+    values = [['',true],[nil,true],['2',false],[' ',false],['あ',false]]
+    values.each do |input,res|
+      assert_equal(res,input.blank?,%Q!"#{input}" -> "#{res}"!)
+    end
+  end
+end
